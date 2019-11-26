49cb27593c65dcff5b8970f8c8b309078e355789
maks27 <maks2796@gmail.com>
2019-11-26 03:35:49 +0100

    First Terrain + fix

--- AncientScroll/Assets/Script/CameraMAin.cs
+++ AncientScroll/Assets/Script/CameraMAin.cs
@@ -14,7 +14,7 @@ public class CameraMAin : MonoBehaviour
         Vector3 rotation = transform.eulerAngles;
         rotation.y += Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
         //transform.eulerAngles = player.transform.eulerAngles * rotationSpeed; 
-        transform.position = new Vector3(player.transform.position.x, 2, player.transform.position.z) + offset;
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 3, player.transform.position.z) + offset;
         transform.eulerAngles = rotation;
 
 
