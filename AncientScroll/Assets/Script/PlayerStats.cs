a9d1504a35274e86c3d794925e899a74dd69a751
maks27 <maks2796@gmail.com>
2020-01-08 02:39:59 +0100

    Working Health Bar,statistic,fix

---
+++ AncientScroll/Assets/Script/PlayerStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float hp ;
+    public Slider hpcount;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            Debug.Log("Gameover");
+        }
+        hpcount.value = hp;
+       
+    }
+    public void Damage(int damage)
+    {
+      
+        hp -= damage;
+        Debug.Log(hp);
+    }
+   
+    
+}
---
+++ AncientScroll/Assets/Script/SaveGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+public static class SaveGame {
+    public static void Save (PlayerMovment player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/saveplayer.as";
+
+    }
+}
